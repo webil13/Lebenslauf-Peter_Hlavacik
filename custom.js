8bcbe2942e8cb5e008fb7717ebe300087ba49951
webil13 <p.hlavacik@gmail.com>
2017-08-04 15:15:01 +0200

    Upload RAW files
    som uploudol

---
+++ custom.js
@@ -0,0 +1,25 @@
+// ScrollFire init
+var options = [{
+    selector: '#staggered-test',
+    offset: 200,
+    callback: function(el) {
+      Materialize.showStaggeredList($(el));
+    }
+  },
+  {
+    selector: '.profil-img',
+    offset: 150,
+    callback: function(el) {
+      Materialize.fadeInImage($(el));
+    }
+  },
+];
+Materialize.scrollFire(options);
+
+
+$(document).ready(function() {
+  $('.scrollspy').scrollSpy();
+  $('.modal').modal();
+  $('.materialboxed').materialbox();
+  $('.parallax').parallax();
+});
